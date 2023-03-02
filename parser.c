9d634b8a4b6dc2c83646a159e63c417149977cd8
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2023-03-02 15:57:44 +1100

    fix realloc bug

--- parser.c
+++ parser.c
@@ -6,18 +6,18 @@
 char **sh_parse_tokens(char *line) {
 	char **tokens;
 	size_t tokens_len = SH_TOK_BUFSIZE;
-	char  *token;
 	size_t i = 0;
 
 	tokens = sh_malloc(tokens_len * sizeof(char *));
 
-	token = strtok(line, SH_TOK_DELIMITERS);
+	char *token = strtok(line, SH_TOK_DELIMITERS);
 	while (token) {
 		tokens[i++] = token;
 
 		if (i == tokens_len) {
 			tokens_len *= 2;
-			tokens = sh_realloc(tokens, tokens_len);
+			tokens =
+			    sh_realloc(tokens, tokens_len * sizeof(char *));
 		}
 
 		token = strtok(NULL, SH_TOK_DELIMITERS);
